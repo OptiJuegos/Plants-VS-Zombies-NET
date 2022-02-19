9b489dd97444ab17d31f32050ac2e8ee1328af28
OptiJuegos <94207445+OptiJuegos@users.noreply.github.com>
2022-02-19 17:52:42 -0300

    Add files via upload

---
+++ DotNETPvZ_Shared/Sexy/DEBUG/Debug.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+namespace Sexy
+{
+	
+	internal static class Debug
+	{
+		
+		public static void OutputDebug<T>(T t)
+		{
+#if DEBUG
+			Console.WriteLine(String.Format("[DEBUG] {0}", t.ToString()));
+#endif
+		}
+
+		
+		public static void OutputDebug<T1, T2>(T1 t1, T2 t2)
+		{
+#if DEBUG
+			Console.WriteLine(String.Format("[DEBUG] {0}:{1}",t1.ToString(),t2.ToString()));
+#endif
+		}
+
+
+		public static void ASSERT(bool value)
+		{
+#if DEBUG
+			if (!value) 
+			{
+				throw new ApplicationException("[DEBUG] Assert failed.");
+			}
+			
+#endif
+		}
+	}
+}
